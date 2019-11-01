fa1e990b16b17aa94070143d0b0b9d438ea3bffd
Hannah Bingham <binghan.hb@gmail.com>
2019-11-01 11:06:48 -0500

    Added dashboard for admin area.

---
+++ JPCreationsCornerV2.0/JPCreationsCornerV2.0/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JPCreationsCornerV2._0.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
