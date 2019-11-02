bd6d1c3d1bae5b1329ad0230d5ab3ffccf8c626b
Hannah Bingham <binghan.hb@gmail.com>
2019-11-02 12:15:49 -0500

    Added a fileman folder so that admins have the ability to upload images easier than in original jpcreations project. also added ckeditor so that admins can edit their site easier without haveing to contact the developer.

--- JPCreationsCornerV2.0/JPCreationsCornerV2.0/Models/ViewModels/Pages/PageViewModel.cs
+++ JPCreationsCornerV2.0/JPCreationsCornerV2.0/Models/ViewModels/Pages/PageViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using JPCreationsCornerV2._0.Models.Data;
 
 namespace JPCreationsCornerV2._0.Models.ViewModels.Pages
@@ -29,6 +30,7 @@ namespace JPCreationsCornerV2._0.Models.ViewModels.Pages
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength =3)]
+        [AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
         public bool HasSidebar { get; set; }
--- JPCreationsCornerV2.0/JPCreationsCornerV2.0/Models/ViewModels/Pages/SideBarViewModel.cs
+++ JPCreationsCornerV2.0/JPCreationsCornerV2.0/Models/ViewModels/Pages/SideBarViewModel.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using JPCreationsCornerV2._0.Models.Data;
 
 namespace JPCreationsCornerV2._0.Models.ViewModels.Pages
 {
     public class SideBarViewModel
     {
-
+        
         public int Id  {get;set;}
+        [AllowHtml]
         public string Body { get; set; }
         public SideBarViewModel()
         {
