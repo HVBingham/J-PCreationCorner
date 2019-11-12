967b26a6a1df82872d4e947805d757fb48be4169
Hannah Bingham <binghan.hb@gmail.com>
2019-11-12 12:26:13 -0600

    Fixed bug with editing category names.

--- JPCreationsCornerV2.0/JPCreationsCornerV2.0/Areas/Admin/Controllers/ShopController.cs
+++ JPCreationsCornerV2.0/JPCreationsCornerV2.0/Areas/Admin/Controllers/ShopController.cs
@@ -11,48 +11,82 @@ namespace JPCreationsCornerV2._0.Areas.Admin.Controllers
     public class ShopController : Controller
     {
         // GET: Admin/Shop
-        public ActionResult Index()
+        public ActionResult Categories()
         {
             List<CategoryViewModel> categoryViewModelList;
-            using(Context context = new Context())
+            using (Context context = new Context())
             {
                 categoryViewModelList = context.Categories.ToArray().OrderBy(c => c.Sorting).Select(c => new CategoryViewModel(c)).ToList();
             }
             return View(categoryViewModelList);
         }
-      
+
         [HttpPost]
-        public ActionResult AddNewCategory(string caName )
+        public string AddNewCategory(string catName)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    return View(model);
-            //}
-            //using(Context context = new Context())
-            //{
-            //    string slug;
-            //    CategoryDTO dto = new CategoryDTO();
-            //    dto.Name = model.Name;
-            //    if (string.IsNullOrWhiteSpace(model.Slug))
-            //    {
-            //        slug = model.Name.Replace("", "-").ToLower();
-            //    }
-            //    else
-            //    {
-            //        slug = model.Slug.Replace("", "-").ToLower();
-            //    }
-            //    if(context.Categories.Any(c=>c.Name==model.Name)|| context.Categories.Any(c => c.Slug == model.Slug))
-            //    {
-            //        ModelState.AddModelError("", "That title or slug already exists.");
-            //        return View(model);
-            //    }
-            //    dto.Slug = slug;
-            //    dto.Sorting = 100;
-            //    context.Categories.Add(dto);
-            //    context.SaveChanges();
-            //}
-            //TempData["SM"] = "You have added a new category!";
-            return RedirectToAction("AddNewCategory");
+            string id;
+            using (Context context = new Context())
+            {
+                if (context.Categories.Any(c => c.Name == catName))
+                {
+                    return "titletaken";
+                }
+                CategoryDTO dto = new CategoryDTO();
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+                context.Categories.Add(dto);
+                context.SaveChanges();
+                id = dto.Id.ToString();
+            }
+            return id;
+        }
+
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Context context = new Context())
+            {
+                int count = 1;
+                CategoryDTO dto;
+                foreach (var categoryId in id)
+                {
+                    dto = context.Categories.Find(categoryId);
+                    dto.Sorting = count;
+                    context.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Context context = new Context())
+            {
+                CategoryDTO dto = context.Categories.Find(id);
+                if (dto == null)
+                {
+                    return Content("The Page does not exist.");
+                }
+                context.Categories.Remove(dto);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Categories");
+        }
+        [HttpPost]
+        public string RenameCategory(string  newCatName, int id)
+        {
+            using (Context context = new Context())
+            {
+                if (context.Categories.Any(c => c.Name == newCatName))
+                    return "titletaken";
+                CategoryDTO dto = context.Categories.Find(id);
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+                context.SaveChanges();
+            }
+            return "OK";
         }
     }
 
